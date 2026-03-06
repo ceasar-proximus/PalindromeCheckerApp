fd6a624866695b7f323e98a2357ee211ed4a9943
ceasar-proximus <ceasarthoughts167@gmail.com>
2026-03-06 09:46:53 +0530

    Use case 4 finished

--- PalindromCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromCheckerApp/src/PalindromeCheckerApp.java
@@ -1,19 +1,31 @@
+//version 4.0
+//use case4: Character Array Based Palindrome Check
 import java.util.Scanner;
 
 public class PalindromeCheckerApp{
     public static void main(String[] args){
         System.out.print("Welcome to Palindrome Checker App Management System\n");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string: ");
+        System.out.print("Enter a word: ");
         String input = scanner.nextLine();
-        String reversed = "";
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
+        char[] chars = input.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
+        while (start < end) {
+
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
         }
-        if (input.equals(reversed)) {
-            System.out.println(input + " is a palindrome.");
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
         } else {
-            System.out.println(input + " is not a palindrome.");
+            System.out.println(input + " is NOT a Palindrome.");
         }
         scanner.close();
     }
